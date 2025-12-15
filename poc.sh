a1c9b2dac778ac00212ac8fd22690e75db7e390a
r0lan <98378555+redteam-wtf@users.noreply.github.com>
2025-12-15 23:26:13 +0800

    Update poc.sh

--- poc.sh
+++ poc.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env sh
 # Run from .gitmodules
-printf "\e[41;32m... Running code here ..." >> /dev/stderr
-xcalc&
-open -a Calculator&
+#printf "\e[41;32m... Running code here ..." >> /dev/stderr
+#xcalc&
+#open -a Calculator&
+pwd
+id
