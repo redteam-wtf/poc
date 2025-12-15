11f0c2f1d4738e2472200b4d5407867a997a4744
r0lan <98378555+redteam-wtf@users.noreply.github.com>
2025-12-15 23:43:59 +0800

    Update poc.sh

--- poc.sh
+++ poc.sh
@@ -3,7 +3,5 @@
 #printf "\e[41;32m... Running code here ..." >> /dev/stderr
 #xcalc&
 #open -a Calculator&
-whoami&
-#printf "location is " && pwd& 
-#printf "running id is " && id&
-#id
+id >&2 &
+whoami >&2 &
